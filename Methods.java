d6e3da6d2e5a98d103fd12de563360f730068848
yourmoms-boyfriend <25kvang5883@apps-hcpak12.org>
2023-05-01 18:34:48 -0500

    Methods.

---
+++ Methods.java
@@ -0,0 +1,35 @@
+public class Methods {
+    
+    public static void main (String[] args){
+        
+        int num1 = 8;
+        int num2 = 11;
+
+        int total = num1 + num2;
+
+        System.out.print("The total is: " + total);
+
+        sayHi();
+        // calling the method I made below.
+    }
+
+    // private method means it can only be called from this class.
+
+    private static int add (int num1, int num2){
+        // this is the name of this method. In this case, we're calling it "add"
+
+        return num1 + num2;
+
+    }
+
+    private static void sayHi () {
+
+        System.out.println("Hi");
+
+        sayHi();
+        // You need the () to let Java know that you're calling a method, not using a variable.
+
+        
+    }
+
+}
